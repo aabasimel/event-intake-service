2abf8cd23dd6dfd13236a8b8bd17c78399226bb7
Ahmed Abasimel <abasimelahmed@gmail.com>
2026-01-07 15:53:06 +0100

    feat: update Event model structure

--- backend/event_api/models.py
+++ backend/event_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    id = models.CharField(primary_key=True, max_length=36)
+    received_at = models.DateTimeField()
+    client_ts = models.DateTimeField()
+    event = models.CharField(max_length=64)
+    user_id = models.CharField(max_length=64)
+    metadata = models.JSONField(default=dict)
+    request_id = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id}: {self.event}"
+
